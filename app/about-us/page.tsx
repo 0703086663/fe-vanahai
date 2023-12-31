487e7be4eef8fef745b90251440e5103b4f11068
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2023-12-31 14:51:40 +0700

    Initial commit

---
+++ app/about-us/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>About-us page</div>;
+};
+
+export default Page;
---
+++ app/contact-us/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Contact-us page</div>;
+};
+
+export default Page;
