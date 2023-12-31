4632ab07b95e840a708fa276192d984362d29d7f
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2023-12-31 15:06:12 +0700

    Modify app name, add prettier, fonts

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Menu page</div>;
+  return (
+    <div className="container mx-auto px-4 bg-red-500">
+      <h1>ggg</h1>
+    </div>
+  );
 };
 
 export default Page;
